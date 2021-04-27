dc04c1166439c5580bee1704732911a8a5dddd41
Gilvan Reis <gilvan.reis@meliuz.com.br>
2021-04-27 09:45:58 -0300

    feat: Add request route

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
 
+COPY requirements.txt ./requirements.txt
+RUN pip install -r ./requirements.txt
+
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
 
 app = FastAPI()
 
@@ -11,3 +16,14 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+class Item(BaseModel):
+    q: Optional[str] = None
+
+
+@app.post("/items/{item_id}")
+def request_items(item_id: int, item: Item):
+    host = '127.0.0.1'
+    r = requests.get(f'http://{host}/items/{item_id}', params={'q': item.q})
+    return r.json()
